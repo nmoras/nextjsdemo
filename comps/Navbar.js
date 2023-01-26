9f452d9fb0931151f6fd0ad5f89badaed88dac97
Norma <normamoras@gmail.com>
2023-01-26 05:58:06 -0500

    adding components

---
+++ comps/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div>
+                <h1>Demo List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Demo</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ pages/demo/index.js
@@ -0,0 +1,7 @@
+const Demo = () => {
+    return (  
+        <h1>Demo landing page</h1>
+    );
+}
+ 
+export default Demo;
