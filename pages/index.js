07c8520695dcec71a4135fed8207eaa0015bac66
Norma <normamoras@gmail.com>
2023-01-26 05:48:01 -0500

    adding folder test

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ export default function Home() {
   return (
     <div>
       <h1>homepage</h1>
-      <p>These styles (styles.css) will apply to all pages and components in your application. Due to the global nature of stylesheets, and to avoid conflicts, you may only import them inside pages/_app.js.</p>
-      <p>In development, expressing stylesheets this way allows your styles to be hot reloaded as you edit them—meaning you can keep application state.</p>
+   
     </div>
   )
 }
