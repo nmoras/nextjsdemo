9cb5d3beab2c9fbf8fc070289e14e5d07bfc7da9
Norma <normamoras@gmail.com>
2023-01-26 06:03:18 -0500

    adding components

---
+++ comps/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( 
+        <div>
+             <p>Copyrights 2022 Demo project</p>
+        </div>
+       
+     );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import Navbar from '../comps/Navbar'
+import Footer from '../comps/Footer'
 
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>homepage</h1>
-   
+      <Footer/>
     </div>
   )
 }
