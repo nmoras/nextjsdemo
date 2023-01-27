cc2490ff7d42ca7f8a3b69d19270ec76edd5124e
Norma <normamoras@gmail.com>
2023-01-27 07:11:12 -0500

    adding head component

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,19 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <Link className={styles.btn} href="/demo">
-        See demo list
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
+        <Link className={styles.btn} href="/demo">
+          See demo list
+        </Link>
+      </div>
+    </>
+  
   )
 }
