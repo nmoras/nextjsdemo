78067eb3e456a216b1710e8724beac7f8c85bce4
Norma <normamoras@gmail.com>
2023-01-27 07:02:31 -0500

    redirect 404 to home page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from 'next/router'
 
 
-const Notfound
- = () => {
+const Notfound = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            //router.go(1)
+            router.push('/');           
+        }, 3000)
+    }, [])
     return ( 
         <div className="not-found">
             <h1>
